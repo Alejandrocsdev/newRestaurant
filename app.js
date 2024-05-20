b9f1c1c423a644880a90de65c3683f5a4ced7ffc
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-05-20 14:53:02 +0800

    build: add static files & json for model

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 const router = require('./routes')
+app.use(express.static('public'))
 app.use(router)
 app.listen(port, () => console.log(`http://localhost:${port}`))
