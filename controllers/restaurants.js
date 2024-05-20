4d119bb35a1599c6250263f1b0ff7d7323e2a118
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-05-20 18:00:57 +0800

    add routes comments

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,41 +1,52 @@
 const { restaurantsService } = require('../services')
 
-const restaurants = require('../public/jsons/restaurants.json')
-
 class RestaurantsController {
+  // 取得全部餐廳資訊
   async getAllRestaurants(req, res) {
     try {
+      // 搜尋關鍵字
       const keyword = req.query.search?.trim()
+      // 渲染新增按鈕
       const create = true
+      // 取得全部餐廳
       const restaurants = await restaurantsService.getAll()
+      // 取得搜尋吻合餐廳
       const matched = await restaurantsService.getMatched(restaurants, keyword)
+      // 發送回應
       res.render('home', { restaurants: matched, keyword, create })
     } catch (err) {
       console.error('Error:', err)
       res.status(500).send('Internal Server Error')
     }
   }
-
+  // 取得單間餐廳資訊
   async getRestaurant(req, res) {
     try {
+      // 渲染編輯及刪除按鈕
       const editDelete = true
+      // 取得參數id
       const id = req.params.id
+      // 取得單間餐廳
       const restaurant = await restaurantsService.getById(id)
+      // 檢查餐廳是否存在
       if (!restaurant) {
-        res.status(404).send('Restaurant not found')
+        res.status(404).send('查無此餐廳')
         return
       }
+      // 發送回應
       res.render('restaurant', { restaurant, editDelete })
     } catch (err) {
       console.error('Error:', err)
       res.status(500).send('Internal Server Error')
     }
   }
-
+  // 新增餐廳
   async createRestaurant(req, res) {
     try {
+      // 宣告表單資料
       const { name, name_en, category, image, location, phone, google_map, rating, description } =
         req.body
+      // 根據表單資料新增餐廳
       await restaurantsService.create({
         name,
         name_en,
@@ -47,18 +58,22 @@ class RestaurantsController {
         rating,
         description
       })
+      // 發送回應
       res.redirect('/restaurants')
     } catch (err) {
       console.error('Error:', err)
       res.status(500).send('Internal Server Error')
     }
   }
-
+  // 更新餐廳
   async updateRestaurant(req, res) {
     try {
+      // 宣告表單資料
       const { name, name_en, category, image, location, phone, google_map, rating, description } =
         req.body
+      // 取得參數id
       const id = req.params.id
+      // 根據表單資料更新餐廳
       await restaurantsService.update(
         {
           name,
@@ -73,37 +88,45 @@ class RestaurantsController {
         },
         id
       )
+      // 發送回應
       res.redirect('/restaurants')
     } catch (err) {
       console.error('Error:', err)
       res.status(500).send('Internal Server Error')
     }
   }
-
+  // 刪除餐廳
   async deleteRestaurant(req, res) {
     try {
+      // 取得參數id
       const id = req.params.id
+      // 刪除餐廳
       await restaurantsService.delete(id)
+      // 發送回應
       res.redirect('/restaurants')
     } catch (err) {
       console.error('Error:', err)
       res.status(500).send('Internal Server Error')
     }
   }
-
+  // 渲染新增頁面
   renderCreatePage(req, res) {
     try {
+      // 發送回應
       res.render('create')
     } catch (err) {
       console.error('Error:', err)
       res.status(500).send('Internal Server Error')
     }
   }
-
+  // 渲染編輯頁面(含取得單間餐廳資訊)
   async renderEditPage(req, res) {
     try {
+      // 取得參數id
       const id = req.params.id
+      // 取得單間餐廳
       const restaurant = await restaurantsService.getById(id)
+      // 發送回應
       res.render('edit', { restaurant })
     } catch (err) {
       console.error('Error:', err)
